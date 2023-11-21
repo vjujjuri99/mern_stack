93c2f01126782fc65ba28c95715076c872758afa
vjujjuri99 <vjujjuri99@gmail.com>
2023-11-21 17:13:40 +0530

    changed schema

--- backend/db/schema.js
+++ backend/db/schema.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const UserSchema = mongoose.Schema({
     firstname:      { type: String, required: true },
     lastname:       { type: String, required: true },
-    phone_number:   { type: Number, required: true },
+    phone_number:   { type: String, required: true },
     email:          { type: String, required: true },
     username:       { type: String, required: true },
     password:       { type: String, required: true },
